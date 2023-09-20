81e818b5d4d1f357d4203b54a3be54873719f1b8
David Cho <davcho3@gmail.com>
2023-09-20 17:25:33 -0500

    maven commit

---
+++ src/main/java/com/dc/springazure/SpringazureApplication.java
@@ -0,0 +1,21 @@
+package com.dc.springazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringazureApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Congrats! your app deployed";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringazureApplication.class, args);
+	}
+
+}
